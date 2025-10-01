128adf02b5e6362ba12e2e6c14b36013760df69a
Tomek <tomek.wojak796@gmail.com>
2025-10-01 23:57:30 +0200

    created header

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,20 @@
-import { createElement } from "./helpers.js";
+import { createElement } from "./helpers.min.js";
 
 document.addEventListener("DOMContentLoaded", function () {
 	const root = document.querySelector(".root");
 	const container = document.querySelector(".container");
+	const language = handleUserLanguage();
+	const langNames = { pl: "Polski", en: "English" };
+
+	function handleUserLanguage() {
+		let userLang = navigator.language || navigator.userLanguage;
+
+		if (!userLang || userLang !== "pl" || userLang !== "en") return "pl";
+
+		userLang = userLang.slice(0, 2).toLowerCase();
+
+		return userLang;
+	}
 
 	const createLoginHeader = () => {
 		// logo
@@ -25,20 +37,56 @@ document.addEventListener("DOMContentLoaded", function () {
 			"header__logo-text",
 			"login-header__logo-text",
 		]);
-
 		logoText.textContent = "Stream";
 		logo.append(logoImg, logoText);
 		header.append(logo);
 		// end of logo
-        // language selector
-        const langSelect = createElement("button", ["login-header__lang-select"], {
-            "aria-label": "Wybierz język platformy Stream",
-        });
-        const langPlatformLang = createElement("span", [
-            "login-header__lang-platform-lang",
-        ]);
-        langPlatformLang.textContent = "Polski";
 
-        // end of language selector
+		// language selector
+		const langSelect = createElement("button", ["login-header__lang-select"], {
+			"aria-label": "Wybierz język platformy Stream",
+		});
+		const langPlatformLang = createElement("span", [
+			"login-header__lang-platform-lang",
+		]);
+		const langArrow = createElement("img", ["login-header__lang-arrow"], {
+			src: "./icons/chevron-down.svg",
+			alt: "",
+			width: "24",
+			height: "24",
+		});
+		const langList = createElement("ul", ["login-header__lang-list"]);
+		const langChoiceEn = createElement("li", ["login-header__lang-choice"], {
+			"data-lang": "en",
+		});
+		const langChoicePl = createElement("li", ["login-header__lang-choice"], {
+			"data-lang": "pl",
+		});
+
+		langChoiceEn.textContent = "English";
+		langChoicePl.textContent = "Polski";
+		langPlatformLang.textContent = langNames[language];
+
+		langSelect.addEventListener("click", () => {
+			handleLangSelect.call(langSelect);
+		});
+
+		langList.append(langChoiceEn, langChoicePl);
+		langSelect.append(langPlatformLang, langArrow, langList);
+		header.append(langSelect);
+
+		return header;
+		// end of language selector
 	};
+
+	function handleLangSelect(e) {
+		const selectBtn = this;
+		selectBtn.classList.toggle("active");
+		// if (e.target.matches(".login-header__lang-choice")) {
+		// 	const selectedLang = e.target.dataset?.lang;
+		// 	langText.textContent = selectedLang;
+		// }
+	}
+
+	container.append(createLoginHeader());
 });
